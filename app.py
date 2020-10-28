9fe75f035ad7ecef2f3988248f44a82f917630d9
mirumcer <sduncan13579@gmail.com>
2020-10-28 09:37:52 -0700

    add index page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
