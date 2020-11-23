7585d68158b60d3910927f91452c31b3c86c6faa
mirumcer <sduncan13579@gmail.com>
2020-11-23 09:33:37 -0800

    add login cookie +jwt

--- docs/login.js
+++ docs/login.js
@@ -1,6 +1,5 @@
 const baseurl = "http://127.0.0.1:5000"
 
-var jwt_token
 
 $(document).ready(function() {
     var loginform = document.getElementById('myform');
@@ -9,14 +8,14 @@ $(document).ready(function() {
         console.log("submit processing")
         formData = $(myform).serializeArray()
         console.log(formData);
-        var fetch_url = baseurl.concat("/auth")
+        const plant_url = baseurl.concat("/auth")
         formData = { "username": formData[0].value, "password": formData[1].value }
 
         console.log(formData)
         formData = JSON.stringify(formData)
         console.log(formData)
 
-        fetch(fetch_url, {
+        fetch(plant_url, {
             method: "post",
             mode: 'cors',
             credentials: 'same-origin',
@@ -25,9 +24,10 @@ $(document).ready(function() {
         }).then(res => {
             data = res.json().then(data => {
                 console.log("request complete ", data)
-                jwt_token = data["access_token"]
+                var jwt_token = data["access_token"]
                 console.log("jwt token:", jwt_token)
-                document.cookie = "jwt_token=".concat(jwt_token)
+                token = "JWT ".concat(jwt_token)
+                document.cookie = "jwt_token=".concat(token)
                 window.location.href = "dashboard.html"
             })
 
