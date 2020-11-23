8e211cb1ed2c45a650153ee893377ee03a1723c0
mirumcer <sduncan13579@gmail.com>
2020-11-23 12:56:51 -0800

    display plants

--- docs/dashboard.js
+++ docs/dashboard.js
@@ -6,30 +6,65 @@ $(document).ready(function() {
     //Check if JWT cookie is set an valid
     //if not go back to login
     var cookie = document.cookie
+    if (!cookie) {
+        window.location.href = "login.html"
+    }
     jwt_token = cookie.split('=')[1]
     console.log("jwt Cookie", jwt_token)
 
     //JWT is valid
     //get the plants and display them
+    get_plants()
+})
 
-    display_plants()
+function get_plants() {
+    const fetch_url = baseurl.concat("/plants")
 
-    function display_plants() {
-        const fetch_url = baseurl.concat("/plants")
+    fetch(fetch_url, {
+        method: "GET",
+        credentials: 'include',
 
-        fetch(fetch_url, {
-            method: "GET",
-            credentials: 'include',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `jwt ${jwt_token}`
+        }
 
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `jwt ${jwt_token}`
+    }).then(res => {
+        data = res.json().then(data => {
+            console.log("request complete ", data)
+            if (res.ok) {
+                display_plants(data);
             }
 
-        }).then(res => {
-            data = res.json().then(data => {
-                console.log("request complete ", data)
-            })
         })
+    })
+}
+
+//add the plants to the document page
+function display_plants(data) {
+    var getUrl = window.location;
+    var currentUrl = getUrl.protocol + "//" + getUrl.host + "/"
+    plant_container = document.getElementById('plant_container')
+    for (plant in data) {
+        console.log(data[plant])
+        html_plant = build_plant(data[plant])
+        plant_container.appendChild(html_plant)
+
+
     }
-})
+}
+
+//Build the Plant document node
+function build_plant(plant) {
+    var getUrl = window.location;
+    var currentUrl = getUrl.protocol + "//" + getUrl.host + "/"
+    var src_html = document.getElementById('plant_card').outerHTML;
+    var html_plant = document.createElement('div')
+    html_plant.innerHTML = src_html
+
+    plant_img = html_plant.getElementsByTagName('img')[0]
+    console.log(plant_img)
+    plant_img.src = currentUrl + plant['img_path']
+
+    return html_plant
+}
