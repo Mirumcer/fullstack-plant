02f6e3d503fb931aa5d4b4458b8bedb144dd0be3
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-24 19:52:19 -0800

    Add files via upload

---
+++ docs/contactus.js
@@ -0,0 +1,33 @@
+const baseurl = "http://127.0.0.1:5000"
+
+
+$(document).ready(function() {
+    var contactform = document.getElementById('contactform');
+
+    contactform.addEventListener("submit", function(event) {
+        console.log("submit processing")
+        formData = $(contactform).serializeArray()
+        console.log(formData);
+        const contactus_url = baseurl.concat("/feedback")
+        formData = { "name": formData[0].value, "email": formData[1].value, "message": formData[2].value }
+
+        console.log(formData)
+        formData = JSON.stringify(formData)
+        console.log(formData)
+
+        fetch(contactus_url, {
+            method: "post",
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: { "Content-Type": "application/json" },
+            body: formData,
+        }).then(res => {
+            data = res.json().then(data => {
+                console.log("request complete ", data)
+                window.location.href = "contactus.html"
+            })
+
+        })
+        event.preventDefault()
+    })
+})
