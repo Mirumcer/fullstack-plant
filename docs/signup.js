59b46bc196c216de196abcb56fa6d24bf028bd41
mirumcer <sduncan13579@gmail.com>
2020-11-29 11:41:03 -0800

    link signup

---
+++ docs/signup.js
@@ -0,0 +1,41 @@
+const baseurl = "http://127.0.0.1:5000"
+
+
+$(document).ready(function() {
+    var loginform = document.getElementById('myform');
+
+    loginform.addEventListener("submit", function(event) {
+        console.log("submit processing")
+            //formData = { "username": username, "password": password }
+        formData = $(myform).serializeArray()
+        const plant_url = baseurl.concat("/new_user")
+        formData = { "username": formData[0].value, "password": formData[1].value }
+
+        console.log(formData)
+        formData = JSON.stringify(formData)
+        console.log(formData)
+
+        fetch(plant_url, {
+            method: "post",
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: { "Content-Type": "application/json" },
+            body: formData,
+        }).then(res => {
+            data = res.json().then(data => {
+                console.log("request complete ", data)
+                if (!res.ok) {
+                    console.log(res.ok)
+                        //var invalid_message = document.getElementById("invalid_message")
+                        //invalid_message.style.visibility = 'visible'
+                    return
+                }
+                console.log("changing to login window")
+                window.location.href = "login.html"
+            })
+            event.preventDefault()
+
+        })
+        event.preventDefault()
+    })
+})
